c7a1d7e9dd697e832c18dced351ccff1e97225ea
malachai <xogmldntmd@naver.com>
2023-01-09 15:13:01 +0900

    Jenkins Container Deployment +updated: run script

--- jenkins/sbin/run.sh
+++ jenkins/sbin/run.sh
@@ -3,4 +3,4 @@
 set -e
 
 sudo docker build -t jenkins/jenkins:custom /home/ec2-user/server-test/jenkins
-sudo docker run -d -it -u root -v /home/ec2-user/server_test/jenkins/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 9000:8080 -p 50000:50000 --name jenkins_container jenkins/jenkins:custom /bin/bash
+sudo docker run -d -it -u root -v /home/ec2-user/server_test/jenkins/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 80:8080 -p 50000:50000 --name jenkins_container jenkins/jenkins:custom /bin/bash
