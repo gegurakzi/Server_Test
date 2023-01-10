a009976b45764eb29b8d787def7fa76549825c44
malachai <xogmldntmd@naver.com>
2023-01-10 14:36:14 +0900

    Hotfix +updated: run script path

--- jenkins/sbin/run.sh
+++ jenkins/sbin/run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sudo docker build -t jenkins/jenkins:custom /home/ec2-user/server-test/jenkins
-sudo docker run -d -it -u root -v /home/ec2-user/server-test/jenkins/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 9090:8080 -p 50000:50000 --name jenkins_container jenkins/jenkins:custom /bin/bash
+sudo docker build -t jenkins/jenkins:custom ~/server-test/jenkins
+sudo docker run -d -it -u root -v ~/server-test/jenkins/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 9090:8080 -p 50000:50000 --name jenkins_container jenkins/jenkins:custom /bin/bash
